049b2cbebb84e87f14d34bee2b11ee6aff1c0c79
Nicodragon25 <nicolasariellaure@gmail.com>
2025-12-08 16:12:01 -0300

    Add inmortality command cheat

--- Assets/Scripts/DEBUG/Console/Commands/InvincibilityCommand.cs
+++ Assets/Scripts/DEBUG/Console/Commands/InvincibilityCommand.cs
@@ -1,3 +1,4 @@
+using DEBUG.Cheats;
 using DEBUG.Console;
 using UnityEngine;
 
@@ -6,7 +7,37 @@ public class InvincibilityCommand : ConsoleCommand
 {
     public override bool Execute(string[] args)
     {
+        CheatsManager cheatsManager = FindObjectOfType<CheatsManager>();
+        if (cheatsManager == null)
+        {
+            Debug.LogError($"Couldn't Find CheatManager");
+            return false;
+        }
+
+        if (args.Length != 1)
+        {
+            Debug.LogError($"Invalids arg count, should be exactly one. (1/0; true/false)");
+            return false;
+        }
+
+
+        args[0].ToLower();
         Debug.Log($"ToggleGod: {args[0]}");
+        switch (args[0])
+        {
+            case "1":
+            case "true":
+                Debug.Log($"INVINCIBLE");
+                cheatsManager.ToggleInvincibility(true);
+                break;
+
+            case "0":
+            case "false":
+                Debug.Log($"Vulnerable");
+                cheatsManager.ToggleInvincibility(false);
+                break;
+        }
+
         return true;
     }
 }
