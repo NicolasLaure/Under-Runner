a2f953761f0faea0a47bfe2f59f155951ec27a00
Maxi Feldman <maximiliano.a.feldman@gmail.com>
2025-12-14 16:46:00 -0300

    Add new motor handling classes

---
+++ Assets/Scripts/Events/ScriptableObjects/MotorFlamesChannelSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using VFX;
+
+namespace Events.ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "Events/Motor Flames", fileName = "onNewMotorFlames")]
+    public class MotorFlamesChannelSO : EventChannelSO<MotorFlamesData>
+    {
+    }
+}
