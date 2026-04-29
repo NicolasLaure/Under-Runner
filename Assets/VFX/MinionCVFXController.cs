2aa5e1b989f0aeb303862c752725d85b77ddd4d7
FabriLM98 <fabrizzio.morandi@estudiante.imagecampus.edu.ar>
2026-04-29 18:07:29 -0300

    Encapsulated Minion C Logic

---
+++ Assets/VFX/MinionCVFXController.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionCVFXController : MonoBehaviour
+{
+    [SerializeField] Animator minionAnim;
+    [SerializeField] Animator redBeamAnim;
+    [SerializeField] GameObject redBeam;
+    [SerializeField] GameObject goForScale;
+    [SerializeField] Transform aimTransform;
+    [SerializeField] float timeForBeamToAppear;
+    [SerializeField] float beamMaxLength;
+    [SerializeField] float timeForBeamToTurnOn;
+    [SerializeField] float vortexAnimationDuration = 0.28f;
+    float flickerDuration = 1;
+    [SerializeField] GameObject damagingBeamGO;
+    bool isFlickering = false;
+
+    public delegate void AnimationEvent(MinionCVFXController vfxController);
+    public static event AnimationEvent OnFinishedWarning;
+
+    public void SetBeamMaxLength(float length)
+    {
+        beamMaxLength = length;
+    }
+    public void Aim(bool aimDown)
+    {
+        minionAnim.SetBool("isAimingDown", aimDown);
+        minionAnim.SetTrigger("Aim");
+        StartCoroutine(TurnOnRedBeamCoroutine());
+    }
+    
+
+    public void Charge(float warningDuration)
+    {
+        isFlickering = true;
+        this.flickerDuration = warningDuration;
+        StartCoroutine(FlickerCoroutine());
+    }
+
+    public void Release()
+    {
+        redBeam.SetActive(false);
+        minionAnim.SetTrigger("Release");
+        damagingBeamGO.SetActive(false);
+        damagingBeamGO.SetActive(true);
+    }
+    public void Leave()
+    {
+        minionAnim.SetTrigger("Leave");
+    }
+    
+    IEnumerator TurnOnRedBeamCoroutine()
+    {
+        yield return new WaitForSeconds(timeForBeamToAppear);
+        aimTransform.position = new Vector3(beamMaxLength, aimTransform.position.y, aimTransform.position.z);
+        float currentLength = 0;
+        redBeam.SetActive(true);
+        float timer = 0;
+        while (timer < timeForBeamToTurnOn)
+        {
+            timer += Time.deltaTime;
+            currentLength = Mathf.Clamp((timer / timeForBeamToTurnOn) * beamMaxLength, 0, beamMaxLength);
+            goForScale.transform.localScale = new Vector3(goForScale.transform.localScale.x, goForScale.transform.localScale.y, currentLength);
+            yield return null;
+        }
+        goForScale.transform.localScale = new Vector3(goForScale.transform.localScale.x, goForScale.transform.localScale.y, beamMaxLength);
+
+    }
+    IEnumerator FlickerCoroutine()
+    {
+        minionAnim.SetTrigger("Charge");
+        bool hasStartedCharging = false;
+        float timer = 0;
+        flickerDuration -= vortexAnimationDuration;
+        while (timer < flickerDuration)
+        {
+            timer += Time.deltaTime;
+            float x = timer / flickerDuration;
+            float curveValue = Mathf.Sin(Mathf.Pow(x, 2) * 100) * -1;
+            redBeam.SetActive(curveValue > 0);
+            if (!hasStartedCharging && timer > flickerDuration * 0.5f)
+            {
+                hasStartedCharging = true;
+                redBeamAnim.SetTrigger("Charge");
+            }
+            yield return null;
+        }
+        redBeam.SetActive(false);
+        redBeamAnim.SetTrigger("Release");
+        yield return new WaitForSeconds(vortexAnimationDuration);
+        if(OnFinishedWarning != null)
+        {
+            OnFinishedWarning(this);
+        }
+    }
+}
--- Assets/VFX/TestMinionC.cs
+++ Assets/VFX/TestMinionC.cs
@@ -6,95 +6,29 @@ using UnityEngine.Rendering.Universal;
 
 public class TestMinionC : MonoBehaviour
 {
-    [SerializeField] Animator minionAnim;
-    [SerializeField] Animator redBeamAnim;
-    [SerializeField] GameObject redBeam;
-    [SerializeField] GameObject goForScale;
-    [SerializeField] Transform aimTransform;
-    [SerializeField] float delayForBeamToAppear;
-    [SerializeField] float beamMaxLength;
-    [SerializeField] float timeForBeamToTurnOn;
-    [SerializeField] float timeForEachFlicker;
-    [SerializeField] float flickerDuration;
-    [SerializeField] float delayForFlickering;
-    [SerializeField] float delayAfterFlickering;
-    [SerializeField] GameObject damagingBeamGO;
-    
+    [SerializeField] MinionCVFXController vfxController;
+
+    private void OnEnable()
+    {
+        MinionCVFXController.OnFinishedWarning += Release;
+    }
+    private void OnDisable()
+    {
+        MinionCVFXController.OnFinishedWarning -= Release;
+    }
+    public void Aim()
+    {
+        vfxController.Aim(false);
+    }
     public void Charge()
     {
-        minionAnim.SetTrigger("Charge");
-        
-        Invoke("TurnOnRedBeam", delayForBeamToAppear);
-        Invoke("StartFlickering", delayForFlickering);
+        vfxController.Charge(2f);
     }
-    public void Release()
+    void Release(MinionCVFXController vfxController)
     {
-        
-    }
-    public void TurnOnRedBeam()
-    {
-        StartCoroutine(TurnOnRedBeamCoroutine());
-    }
-    void StartFlickering()
-    {
-       
-        StartCoroutine(FlickerCoroutine());
-    }
-    IEnumerator TurnOnRedBeamCoroutine()
-    {
-        aimTransform.position = new Vector3(beamMaxLength, aimTransform.position.y, aimTransform.position.z);
-        float currentLength = 0;
-        redBeam.SetActive(true);
-        float timer = 0;
-        while(timer < timeForBeamToTurnOn) 
+        if(vfxController == this.vfxController)
         {
-            timer += Time.deltaTime;
-            currentLength = Mathf.Clamp((timer / timeForBeamToTurnOn) * beamMaxLength,0,beamMaxLength);
-            goForScale.transform.localScale = new Vector3(goForScale.transform.localScale.x, goForScale.transform.localScale.y, currentLength);
-            yield return null;
+            vfxController.Release();
         }
-        goForScale.transform.localScale = new Vector3(goForScale.transform.localScale.x, goForScale.transform.localScale.y, beamMaxLength);
-        
-    }
-    IEnumerator FlickerCoroutine()
-    {
-        bool hasStartedCharging = false;
-        float timer = 0;
-        while(timer < flickerDuration)
-        {
-            timer += Time.deltaTime;
-            float x = timer / flickerDuration;
-            float curveValue = Mathf.Sin(Mathf.Pow(x, 2) * 100) * -1;
-            redBeam.SetActive(curveValue > 0);
-            if(!hasStartedCharging && timer > flickerDuration * (2f/3f))
-            {
-                hasStartedCharging = true;
-                redBeamAnim.SetTrigger("Charge");
-            }
-            yield return null;
-        }
-
-
-
-        redBeamAnim.SetTrigger("Release");
-        float currentLength = 0;
-        redBeam.SetActive(true);
-        timer = 0;
-        while (timer < timeForBeamToTurnOn)
-        {
-            timer += Time.deltaTime;
-            currentLength = Mathf.Clamp((timer / timeForBeamToTurnOn) * beamMaxLength, beamMaxLength,0);
-            goForScale.transform.localScale = new Vector3(goForScale.transform.localScale.x, goForScale.transform.localScale.y, currentLength);
-            yield return null;
-        }
-        goForScale.transform.localScale = new Vector3(goForScale.transform.localScale.x, goForScale.transform.localScale.y, 0);
-        
-        yield return new WaitForSeconds(0.4f);
-        redBeam.SetActive(false);
-        minionAnim.SetTrigger("Release");
-        damagingBeamGO.SetActive(false);
-        damagingBeamGO.SetActive(true);
-
-
     }
 }
